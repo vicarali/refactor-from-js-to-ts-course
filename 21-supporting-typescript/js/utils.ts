92f4b84e678bb7f689b461e126c203c8df50e838
vicarali <vicarali@gmail.com>
2023-03-05 16:33:29 +0100

    Fix TS lint problems from 21-supporting-typescript

--- 21-supporting-typescript/js/utils.ts
+++ 21-supporting-typescript/js/utils.ts
@@ -1,20 +1,20 @@
-export function countWords(str) {
+export function countWords(str: string) {
   return str.split(" ").length;
 }
 
-export function countChars(str) {
+export function countChars(str: string) {
   return str.split("").length;
 }
 
-export function show(element) {
+export function show(element: HTMLElement) {
   element.classList.remove("hidden");
 }
 
-export function hide(element) {
+export function hide(element: HTMLElement) {
   element.classList.add("hidden");
 }
 
-export function createUser(form) {
+export function createUser(form: HTMLFormElement) {
   return new Promise((resolve) => {
     const newUser = Object.values(form.elements).reduce((user, element) => {
       if (element.id) {
